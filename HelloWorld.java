f7ec7e0f80e7038edf8725bbb10bab333dc5ef1d
pfeifinator <pfe11001@byui.edu>
2015-10-10 19:58:08 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
   
   public static void main {
 
-  System.out.println("Hola Mundo do GitHub");
+  System.out.println("Hello GitHub World");
   System.out.println("Experimentando...");
 
   }
